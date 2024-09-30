90192398bb6e4bd1f749b2b99ddd8ce6aca918bc
Ryan Vo <38389871+ryyyvo@users.noreply.github.com>
2024-09-30 00:19:02 -0700

    Finish main code. Minor fixes/optimization needed

--- rvo03_lab-1a/schoolsearch.py
+++ rvo03_lab-1a/schoolsearch.py
@@ -28,10 +28,79 @@ def parse_students():
 
     return students
 
-def find_by_StLastName(lastname, students):
+def find_by_StLastName(lastname, students): #R4
     for student in students:
         if student['StLastName'] == lastname:
             print(student['StLastName'], student['StFirstName'], student['Grade'], student['Classroom'], student['TLastName'], student['TFirstName'])
+
+def find_by_StLastName_bus(lastname, students): #R5
+    for student in students: 
+        if student['StLastName'] == lastname:
+            print(student['StLastName'], student['StFirstName'], student['Bus'])
+
+def find_by_TLastName(lastname, students): #R6
+    for student in students:
+        if student['TLastName'] == lastname:
+            print(student['StLastName'], student['StFirstName'])
+
+def find_by_grade(grade, students): #R7
+    for student in students:
+        if student['Grade'] == grade:
+            print(student['StLastName'], student['StFirstName'])
+
+def find_by_bus(route, students): #R8
+    for student in students:
+        if student['Bus'] == route:
+            print(student['StLastName'], student['StFirstName'], student['GPA'], student['Classroom'])
+
+def find_by_grade_high_low(grade, lowhigh, students): #R9
+    temp = False
+    tempGPA = False
+    for student in students:
+        if student['Grade'] == grade:
+            if lowhigh == "High" or lowhigh == "H":
+                if not tempGPA:
+                    tempGPA = 0 
+                if float(student['GPA']) >= tempGPA:
+                    tempGPA = float(student['GPA'])
+                    temp = student
+            elif lowhigh == "Low" or lowhigh == "L":
+                if not tempGPA:
+                    tempGPA = 5
+                if float(student['GPA']) <= tempGPA:
+                    tempGPA = float(student['GPA'])
+                    temp = student
+    if not temp:
+        print('No students in the grade')
+        return
+    print(temp['StLastName'], temp['StFirstName'], temp['GPA'], temp['TLastName'], temp['TFirstName'], temp['Bus'])
+                    
+
+def find_by_average(grade, students): #R10
+    totalGP = 0
+    numberOfStudents = 0
+    for student in students:
+        if student['Grade'] == grade:
+            totalGP += float(student['GPA'])
+            numberOfStudents += 1
+    if numberOfStudents == 0:
+        print('No students in the grade')
+        return
+    gradeLevelGPA = round(totalGP/numberOfStudents, 2)
+    print(grade, gradeLevelGPA)
+
+def info(students): #R11
+    totalStudentsPerGrade = {'0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0}
+    for student in students:
+        totalStudentsPerGrade[str(student['Grade'])] += 1
+    print('0: ' + str(totalStudentsPerGrade['0']) + ", " \
+          '1: ' + str(totalStudentsPerGrade['1']) + ", " \
+          '2: ' + str(totalStudentsPerGrade['2']) + ", " \
+          '3: ' + str(totalStudentsPerGrade['3']) + ", " \
+          '4: ' + str(totalStudentsPerGrade['4']) + ", " \
+          '5: ' + str(totalStudentsPerGrade['5']) + ", " \
+          '6: ' + str(totalStudentsPerGrade['6'])
+          )
 
 def main():
     if not os.path.exists(STUDENTS_PATH):
@@ -45,12 +114,28 @@ def main():
         instruction = user_input.split()
 
         if len(instruction) == 1:
-            if (instruction[0] == "Q") or (instruction[0] == "Quit"):
+            if (instruction[0] == "Q") or (instruction[0] == "Quit"): #R12
                 exit(0)
+            elif (instruction[0] == "I") or (instruction[0] == "Info"): #R11
+                info(students)
 
         elif len(instruction) == 2:
-            if (instruction[0] == "S") or (instruction[0] == "Student"):
+            if (instruction[0] == "S") or (instruction[0] == "Student"): #R4
                 find_by_StLastName(instruction[1], students)
+            elif (instruction[0] == "T") or (instruction[0] == "Teacher"): #R6
+                find_by_TLastName(instruction[1], students)
+            elif (instruction[0] == "G") or (instruction[0] == "Grade"): #R7
+                find_by_grade(int(instruction[1]), students)
+            elif (instruction[0] == "B") or (instruction[0] == "Bus"): #R8
+                find_by_bus(int(instruction[1]), students)
+            elif (instruction[0] == "A") or (instruction[0] == "Average"): #R10
+                find_by_average(int(instruction[1]), students)
+
+        elif len(instruction) == 3:
+            if (instruction[0] == "S" or instruction[0] == "Student") and (instruction[2] == "B" or instruction[2] == "Bus"): #R5
+                find_by_StLastName_bus(instruction[1], students)
+            if (instruction[0] == "G") or (instruction[0] == "Grade"): #R9
+                find_by_grade_high_low(int(instruction[1]), instruction[2], students)
 
 
 
